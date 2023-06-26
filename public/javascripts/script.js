4b5061cec33798de507a651fc524eecdc983e6b6
Justin-Guimond <trustinjusting@gmail.com>
2023-06-26 15:13:07 -0400

    Commit 7 - JS function to underline current nav page

---
+++ public/javascripts/script.js
@@ -0,0 +1,20 @@
+$(() => {
+
+const $a = $('div a').hover(function (){  
+    $(this).css({'text-decoration':'underline'})
+    
+    }, function() {
+        $(this).css({'text-decoration':'none'})
+    });
+
+const currentUrl = window.location.href;
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(function(link) {
+    if (link.href === currentUrl) {
+        link.classList.add('current');
+    }
+})
+
+});

2ad3af4a9b968f4ede53d4f97bb61ebdb73b3187
Justin-Guimond <trustinjusting@gmail.com>
2023-06-27 14:12:59 -0400

    Commit 14 - Added elevator icon to tab

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,31 @@
 $(() => {
 
-const $a = $('div a').hover(function (){  
-    $(this).css({'text-decoration':'underline'})
-    
-    }, function() {
-        $(this).css({'text-decoration':'none'})
+    const $a = $('div a').hover(function (){  
+        $(this).css({'text-decoration':'underline'})
+        
+        }, function() {
+            $(this).css({'text-decoration':'none'})
+        });
+
+
+    const currentUrl = window.location.href;
+
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(function(link) {
+        if (link.href === currentUrl) {
+            link.classList.add('current');
+        }
     });
 
+    // function confirmDelete() {
+    //     const result = confirm('Are you sure you want to delete?')
 
-const currentUrl = window.location.href;
-
-const navLinks = document.querySelectorAll('nav a');
-
-navLinks.forEach(function(link) {
-    if (link.href === currentUrl) {
-        link.classList.add('current');
-    }
-});
-
+    //     if (result) {
+    //             Sessions.findByIdAndRemove(req.params.id);
+    //             res.redirect('/');   
+    //     } else {
+    //         res.redirect('/');
+    //     };
+    // };
 });

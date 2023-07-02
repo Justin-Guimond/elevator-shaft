2cd84b921823c611d55b72ff832b57c1b09da3f4
Justin-Guimond <trustinjusting@gmail.com>
2023-07-02 13:33:57 -0400

    Commit 24 - date output correct time zone

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,24 +1,24 @@
 $(() => {
 
-    const $a = $('div a').hover(function (){  
-        $(this).css({'text-decoration':'underline'})
-        
-        }, function() {
-            $(this).css({'text-decoration':'none'})
-        });
+    const $a = $('div a').hover(function () {
+        $(this).css({ 'text-decoration': 'underline' })
+
+    }, function () {
+        $(this).css({ 'text-decoration': 'none' })
+    });
 
 
     const currentUrl = window.location.href;
 
     const navLinks = document.querySelectorAll('nav a');
 
-    navLinks.forEach(function(link) {
+    navLinks.forEach(function (link) {
         if (link.href === currentUrl) {
             link.classList.add('current');
         }
     });
 
-    document.getElementById('deleteButton').addEventListener('click', function(event) {
+    document.getElementById('deleteButton').addEventListener('click', function (event) {
         event.preventDefault();
 
         if (confirm('Are you sure you want to delete?')) {
